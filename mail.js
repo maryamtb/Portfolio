c1ab74942648b52baaec430833c9a8f550d52b7f
maryamsgithub <maryamtb.17@gmail.com>
2019-08-22 11:22:54 +0200

    Contact Form Successful

---
+++ mail.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
+
+const auth = {
+    auth: {
+        api_key: process.env.API_KEY, 
+        domain: process.env.DOMAIN
+    }
+};
+
+const transporter = nodemailer.createTransport(mailGun(auth));
+
+const sendMail = (name, lastname, subject, email, text, cb) => {
+    const mailOptions = {
+        name: name,
+        lastname: lastname,
+        subject: subject,
+        from: email, 
+        to: 'maryamtb.17@gmail.com', 
+        text
+    };
+
+    transporter.sendMail(mailOptions, function (err, data) {
+        if (err) {
+            cb(err, null);
+        } else {
+            cb(err, data);
+        }
+    });
+}
+
+module.exports = sendMail;
--- server.js
+++ server.js
@@ -1,25 +1,49 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const sendMail = require('./mail.js');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+const PORT = 8080;
+
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+
+app.post('/email', (req, res) => {
+    const { name, lastname, subject, email, text} = req.body;
+    console.log('Data: ', req.body);
+
+    sendMail(name, lastname, subject, email, text, function(err, data) {
+        if (err) {
+            console.log('ERROR: ', err);
+            return res.status(500).json({ message: err.message || 'Internal Error' });
+        } else { 
+            console.log('Email sent!');
+            return res.json({ message: 'Email sent!' });
+        }
+    });
 });
 
-app.get('/index.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname,'views','/index.html'));
+});
+
+app.get('/email/sent', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views','emailMessage.html'));
 });
 
 app.get('/webapps.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/webapps.html'));
+    res.sendFile(path.join(__dirname,'views','/webapps.html'));
 });
 
 app.get('/iOSapps.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/iOSapps.html'));
+    res.sendFile(path.join(__dirname,'views','/iOSapps.html'));
 });
 
 app.get('/contact.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/contact.html'));
+    res.sendFile(path.join(__dirname,'views','/contact.html'));
 });
 
-app.listen(8080);
+app.listen(PORT, () => console.log('Server is starting on PORT', PORT));
