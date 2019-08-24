89ffbaea8ffc907a16cfc583e29cee2bc2b5e6ab
maryamsgithub <maryamtb.17@gmail.com>
2019-08-24 22:45:53 +0200

    debugging continues

--- mail.js
+++ mail.js
@@ -1,14 +1,14 @@
-const nodemailer = require('nodemailer');
-const mailGun = require('nodemailer-mailgun-transport');
+const nodemailer = require('nodemailer')
+const mailGun = require('nodemailer-mailgun-transport')
 
 const auth = {
     auth: {
-        api_key: process.env.API_KEY, 
-        domain: process.env.DOMAIN
+        API_KEY: process.env.API_KEY, 
+        DOMAIN: process.env.DOMAIN
     }
-};
+}
 
-const transporter = nodemailer.createTransport(mailGun(auth));
+const transporter = nodemailer.createTransport(mailGun(auth))
 
 const sendMail = (name, lastname, subject, email, text, cb) => {
     const mailOptions = {
@@ -18,15 +18,15 @@ const sendMail = (name, lastname, subject, email, text, cb) => {
         from: email, 
         to: 'maryamtb.17@gmail.com', 
         text
-    };
+    }
 
     transporter.sendMail(mailOptions, function (err, data) {
         if (err) {
-            cb(err, null);
+            cb(err, null)
         } else {
-            cb(err, data);
+            cb(err, data)
         }
-    });
+    })
 }
 
-module.exports = sendMail;
+module.exports = sendMail
